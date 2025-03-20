ad656aac6f4969f1cf5fec49294dee23401e4ace
klismanmelo <klismanfdm@gmail.com>
2025-03-19 23:12:26 -0300

    Settings part 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { SettingsTabs } from "./components/SettingsTabs";
 export default function Home() {
 
   return (
-    <h1>Hello Word</h1>
-  );
+    <>
+      <h1 className="to-zinc-900 text-3xl font-medium">Settings</h1>
+
+      <SettingsTabs />
+    </>
+  )
 }
